018bc91f4c3750b93a9f6651f9aeca1c693d1430
jochensteinmetz <jochen.steinmetz@web.de>
2018-12-15 21:44:52 +0100

    final

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+list=[1,1,3,7,2,4]
+np_list=np.array(list)
+print(np_list)
+np_list=np_list+5
+print(np_list)
+
+data=pd.read.csv("csv/input.csv")
+print(data)
